869bd310b0dc00270a542d52854b3bf3ccc72d8c
Tuannm <ngmt2018@gmail.com>
2023-01-18 19:55:40 +0700

    init

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+function Example() {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
+      (res) => res.json()
+    )
+  );
+
+  if (isLoading) {
+    return 'Loading...';
+  }
+
+  if (error) {
+    return 'An error has occurred: ' + error.message;
+  }
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+}
